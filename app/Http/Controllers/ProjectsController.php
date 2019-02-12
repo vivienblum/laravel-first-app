b31548f2730a6a5a2d3c46f08190a770a10c6350
Vivien BLUM <vivien@MacBook-Pro-de-Vivien.local>
2019-02-12 14:31:30 +0100

    :sparkles: Added projects index

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -22,10 +22,7 @@ class ProjectsController extends Controller
 
     public function store() {
 
-      Project::create([
-        'title' => request('title'),
-        'description' => request('description'),
-      ]);
+      Project::create(request(['title', 'description']));
 
       return redirect('/projects');
     }
@@ -44,10 +41,7 @@ class ProjectsController extends Controller
 
     public function update(Project $project) {
 
-      $project->title = request('title');
-      $project->description = request('description');
-
-      $project->save();
+      $project->update(request(['title', 'description']));
 
       return redirect('/projects');
     }
