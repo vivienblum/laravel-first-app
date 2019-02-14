b82dd4350cb43e34feae9afaba12440fe9c979f2
Vivien BLUM <vivien@MacBook-Pro-de-Vivien.local>
2019-02-14 10:50:51 +0100

    :sparkles: Job and queue work with database

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\SendMailable;
+use App\Jobs\SendEmailJob;
 
 class EmailController extends Controller
 {
     public function sendEmail()
     {
-        Mail::to('mail@appdividend.com')->send(new SendMailable());
+        // $emailJob = (new SendEmailJob())->delay(now()->addSeconds(3));
+        //
+        // dispatch($emailJob);
+
+        SendEmailJob::dispatch()
+            ->delay(now()->addSeconds(3));
+
         echo 'email sent';
     }
 }
