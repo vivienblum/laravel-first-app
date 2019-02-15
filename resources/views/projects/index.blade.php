6b2735761949b00aa7e532a4eda8bf53e80b6ece
Vivien BLUM <vivien@MacBook-Pro-de-Vivien.local>
2019-02-15 17:37:46 +0100

    :construction: WIP

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,6 +5,7 @@
 <h1 class="title">Projects</h1>
 
 <button class="button is-primary"><a href="/projects/create">Add Project</a></button>
+<button class="button is-info"><a href="/file">Upload file</a></button>
 
 <div class="box" style="margin-top: 20px;">
   <ul>
